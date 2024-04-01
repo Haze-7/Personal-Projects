8fdd246119912095e7935d98961a68009813119c
Hayes Chiasson <hayeschiasson1@yahoo.com>
2024-04-01 15:26:15 -0500

    React Learning

--- React/react6-renderinglists/src/List.jsx
+++ React/react6-renderinglists/src/List.jsx
@@ -1,12 +1,16 @@
 
 function List() {
 
-    let fruits = ["apple", "orange", "banana", "coconut", "pineapple"];
+    let fruits = [{ID: 1, name:"apple", calories: 95}, 
+                 {ID: 2, name:"orange", calories: 45}, 
+                 {ID: 3, name:"banana", calories:  105}, 
+                 {ID: 4, name:"coconut", calories: 159}, 
+                 {ID: 5, name:"pineapple", calories: 37}];
 
-    fruits.sort();
 
-    let listItems = fruits.map(fruit => <li>{fruit}</li>); // pass in call back, function expression, or arrow function
-   
+     let listItems = fruits.map(fruit => <li key={fruit.ID}> 
+                                                      {fruit.name}: &nbsp;
+                                                     <b>{fruit.calories}</b></li>); //change all names to that of filter array
     return (
         <ol>{listItems}</ol>
     );
@@ -14,7 +18,18 @@ function List() {
 
 export default List 
 
-//Convert Array of Strings => List Elements & Render
+//Original List of fruits:
+
+// let fruits = [{ID: 1, name:"apple", calories: 95}, 
+// {ID: 2, name:"orange", calories: 45}, 
+// {ID: 3, name:"banana", calories:  105}, 
+// {ID: 4, name:"coconut", calories: 159}, 
+// {ID: 5, name:"pineapple", calories: 37}];
+
+
+//SORTING EXAMPLES / PRACTICE
+
+//Convert Array of Strings => Array of List Elements & Render
 // let fruits = ["apple", "orange", "banana", "coconut", "pineapple"];
 
 // fruits.sort();
@@ -24,3 +39,28 @@ export default List
 // return (
 //     <ol>{listItems}</ol>
 // );
+
+    //sort array / objects by their name property in lexi / alphabetical order
+    //fruits.sort((a, b) => a.name.localeCompare(b.name));// write custom sorting function
+    //reverse order(swap a and b): 
+    //fruits.sort((b, a) => b.name.localeCompare(a.name));
+    //sort by calories(): Numeric/ ascending Order
+     ////sort by calories(): Numeric /descending Order
+    //fruits.sort((a,b) => b.calories- a.calories) // swap a and b
+
+//FILTER EXAMPLES / PRACTICE 
+
+    //filter for calories of each fruit, if less than 100, keep
+    //let lowCalFruits = fruits.filter(fruit => fruit.calories < 100); // array of low cal fruits
+
+    // ex: w/ low Cal Fruits filter
+    // let listItems = lowCalFruits.map(lowCalFruit => <li key={lowCalFruit.ID}>
+    //                                         {lowCalFruit.name}: &nbsp;
+    //                                         <b>{lowCalFruit.calories}</b></li>); // pass in call back, function expression, or arrow function
+    //Ex for high calorie filter output
+    //filter for calories of each fruit, greater than / = 100/
+    //let highCalFruits = fruits.filter(fruit=> fruit.calories >= 100); //array of high calorie fruits
+
+    // let listItems = highCalFruits.map(highCalFruit => <li key={highCalFruit.ID}> 
+    //                                                {highCalFruit.name}: &nbsp;
+    //                                                <b>{highCalFruit.calories}</b></li>); //change all names to that of filter array
