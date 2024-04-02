0c9499d8b05d07c178ad4e9afb28855413ce476b
Hayes Chiasson <hayeschiasson1@yahoo.com>
2024-04-02 12:55:34 -0500

    work on bank app

--- BBankExperimentation/src/User.java
+++ BBankExperimentation/src/User.java
@@ -12,13 +12,30 @@ public class User {
 	
 	public User(String username, String password, String securityQuestion, String securityAnswer, int numOfBankAccounts)
 	{
-		
+		this.username = username;
+		this.password = password;
+		this.securityQuestion = securityQuestion;
+		this.securityAnswer = securityAnswer;
+		this.numOfBankAccounts = numOfBankAccounts;
 	}
 	
 	public User()
 	{
+		username = "haze7";
+		password = "Zebra25";
+		securityQuestion = "What is this?";
+		securityAnswer = "It is that";
+		numOfBankAccounts = 3;
+	}
+	
+	public void setUser() {
 		
 	}
+	
+	public void getUser() {
+		
+	}
+	
 	
 
 }
